2cdcb00a367f23f552d41e39cf79a07bd18de906
cristianglobant <cristian.castillo@globant.com>
2020-08-14 03:45:10 -0500

    Rename orignal app

--- OriginalApp.tsx
+++ OriginalApp.tsx
@@ -6,7 +6,7 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
-export default function App() {
+export default function OriginalApp() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
